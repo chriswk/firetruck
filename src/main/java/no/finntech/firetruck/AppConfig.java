78cc3aabb3494d2f37ece5873e18ff31b8fd5f32
chrkolst <christopher.kolstad@finn.no>
2016-06-07 08:38:09 +0200

    Initial setup

---
+++ src/main/java/no/finntech/firetruck/AppConfig.java
@@ -0,0 +1,7 @@
+package no.finntech.firetruck;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfig {
+}
---
+++ src/main/java/no/finntech/firetruck/FireTruckRunner.java
@@ -0,0 +1,9 @@
+package no.finntech.firetruck;
+
+import org.springframework.boot.SpringApplication;
+
+public class FireTruckRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
