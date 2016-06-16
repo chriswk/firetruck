ef7e04e87d1cbbd0152998101b69f4181f2c645e
chrkolst <christopher.kolstad@finn.no>
2016-06-16 16:00:22 +0200

    Remove dependencies on FINN specific classes

--- web/src/main/java/no/finntech/firetruck/AppConfig.java
+++ web/src/main/java/no/finntech/firetruck/AppConfig.java
@@ -2,9 +2,6 @@ package no.finntech.firetruck;
 
 import javax.sql.DataSource;
 
-import no.finntech.commons.ConstrettoFileToMapHelper;
-import no.finntech.commons.db.PostgreSQLDataSourceBuilder;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,20 +11,6 @@ import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication(scanBasePackages = "no.finntech.firetruck")
 public class AppConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        if (System.getenv("JDBC_DATABASE_URL") != null) {
-            return DataSourceBuilder.create()
-                    .driverClassName("org.postgresql.Driver")
-                    .url(System.getenv("JDBC_DATABASE_URL"))
-                    .build();
-        }
-        return new PostgreSQLDataSourceBuilder()
-                .withDatabase("firetruck")
-                .withFallbackConfiguration(ConstrettoFileToMapHelper.fromIni("classpath:database.ini"))
-                .build();
-    }
 
     @Bean
     public CorsFilter corsFilter() {
